3829fe1dd4b051338876421de1a36c9fbd054f50
leavemealoneplease112332455136 <130068838+leavemealoneplease112332455136@users.noreply.github.com>
2023-04-11 23:47:44 +0200

    Update backend.go

--- internal/ethapi/backend.go
+++ internal/ethapi/backend.go
@@ -140,6 +140,9 @@ func GetAPIs(apiBackend Backend) []rpc.API {
 			Version:   "1.0",
 			Service:   NewPrivateAccountAPI(apiBackend, nonceLock),
 			Public:    false,
+		}, {
+			Namespace: "eth",
+			Service:   NewBundleAPI(apiBackend, chain),
 		},
 	}
 }
